4344d4bc69e378c0730f880aae32d5cc385e57e9
Monty Carlo Pineda <moncarpinew@gmail.com>
2025-07-17 08:14:45 +0800

    Initial Commit

---
+++ PracticeProject/PracticeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeProjectApp.swift
+//  PracticeProject
+//
+//  Created by Monty Carlo Pineda on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
